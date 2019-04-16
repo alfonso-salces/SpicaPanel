ead19c39942194cd185003a1518a4828819ed1d5
Arfonzo2k18 <alfonsomtb98@gmail.com>
2019-04-16 18:16:50 +0200

    Inicializando repositorio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', './scss/app.scss'],
 })
 export class AppComponent {
   title = 'spica-panel';
+  constructor(private elementRef: ElementRef) {}
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.querySelector('.menu-izquierdo')
+                                  .addEventListener('click', this.onClick.bind(this));
+  }
+
+  onClick(event) {
+    const claseMenu = event.target.classList;
+    // Selecciona el contenedor
+    const contenedor = document.querySelector('.pagina'),
+          flechaIzq = document.querySelector('.fa-arrow-left'),
+          flechaDer = document.querySelector('.fa-arrow-right');
+
+    if (claseMenu.contains('fa-arrow-left') ) {
+         // cerrar el menĂº lateral
+         contenedor.classList.add('no-menu');
+         event.target.style.display = 'none';
+         (<HTMLElement>document.querySelector('.fa-arrow-right')).style.display = 'block';
+    } else if (claseMenu.contains('fa-arrow-right')) {
+         contenedor.classList.remove('no-menu');
+         event.target.style.display = 'none';
+         (<HTMLElement>document.querySelector('.fa-arrow-left')).style.display = 'block';
+    }
+  }
 }
+
